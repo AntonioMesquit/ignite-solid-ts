1f733c91d978a4df91c509439505e1c0dd3b8eb3
antonio mesquita <antonio109mesquita@gmail.com>
2024-04-17 21:12:35 -0300

    Adding eslint 💅

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,14 +1,13 @@
-import 'dotenv/config'
-import { z } from 'zod'
-
+import "dotenv/config";
+import { z } from "zod";
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3343)
-})
-const _env = envSchema.safeParse(process.env)
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3343),
+});
+const _env = envSchema.safeParse(process.env);
+if (_env.success === false) {
+  console.error(" 👿 Invalid enviroment variables", _env.error.format());
 
-if(_env.success === false) {
-  console.error(' 👿 Invalid enviroment variables' , _env.error.format())
-
-  throw new Error('👿  Invalid enviroment variables.')
-} 
+  throw new Error("👿  Invalid enviroment variables.");
+}
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({
-  port: 3333,
-  host: '0.0.0.0'
-}).then(() => {
-  console.log('server is running 😎')
-})
+app
+  .listen({
+    port: env.PORT,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log("server is running 😎");
+  });
