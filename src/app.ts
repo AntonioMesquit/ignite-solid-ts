da8fcd88f8dc5db54fd228366638ff36f4bf0e1a
antonio mesquita <antonio109mesquita@gmail.com>
2024-04-20 16:09:28 -0300

    Route Creation of a User 🧑‍🦲

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post("/users", async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
-prisma.user.create({
-  data: {
-    name: "Antonio mesqutia",
-    email: "antonio109mesquita@gmail.com",
-  },
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+  return reply.status(201).send();
 });
